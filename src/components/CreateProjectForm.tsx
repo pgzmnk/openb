a924dafeb2e9e29ec27856900e68196f6ef26a20
pgzmnk <pgzmnk@gmail.com>
2023-07-19 12:32:26 -0700

    Introduce map to Form.

--- src/components/CreateProjectForm.tsx
+++ src/components/CreateProjectForm.tsx
@@ -1,11 +1,14 @@
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import { MapGeometryContext } from "@/context/context";
 import { FormProps } from "@/interfaces";
 import { useSession } from "next-auth/react";
 import { v4 as uuidv4 } from "uuid";
 import { Project } from "@/interfaces";
-import React from "react";
+import React, { useContext } from "react";
+import Map from "@/components/Map";
 
 export default function CreateProjectForm({ onSubmit }: FormProps) {
+  const { mapGeometry } = useContext(MapGeometryContext);
   const { data: session } = useSession();
 
   const [project, setProject] = React.useState<Project>({
@@ -18,9 +21,9 @@ export default function CreateProjectForm({ onSubmit }: FormProps) {
   });
 
   // Update project when mapGeometry changes
-  // React.useEffect(() => {
-  //     setProject({ ...project, geometry: JSON.stringify(mapGeometry) });
-  // }, [mapGeometry]);
+  React.useEffect(() => {
+    setProject({ ...project, geometry: JSON.stringify(mapGeometry) });
+  }, [mapGeometry]);
 
   function handleInputChange(
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
@@ -101,32 +104,11 @@ export default function CreateProjectForm({ onSubmit }: FormProps) {
                   htmlFor="cover-photo"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Cover photo
+                  Project area
                 </label>
                 <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                  <div className="text-center">
-                    <PhotoIcon
-                      className="mx-auto h-12 w-12 text-gray-300"
-                      aria-hidden="true"
-                    />
-                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                      <label
-                        htmlFor="file-upload"
-                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                      >
-                        <span>Upload a file</span>
-                        <input
-                          id="file-upload"
-                          name="file-upload"
-                          type="file"
-                          className="sr-only"
-                        />
-                      </label>
-                      <p className="pl-1">or drag and drop</p>
-                    </div>
-                    <p className="text-xs leading-5 text-gray-600">
-                      PNG, JPG, GIF up to 10MB
-                    </p>
+                  <div className="text-center w-full">
+                    <Map />
                   </div>
                 </div>
               </div>
