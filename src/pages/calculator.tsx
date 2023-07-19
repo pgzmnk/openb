a09fc7d32aee474208bf2acd779e55311acc78bf
pgzmnk <pgzmnk@gmail.com>
2023-07-19 12:55:33 -0700

    Introduce calculator.

--- src/pages/calculator.tsx
+++ src/pages/calculator.tsx
@@ -1,3 +1,14 @@
+import { useSession } from "next-auth/react";
+
 export default function Calculator() {
-  return <div>Calculator</div>;
+  const { data: session } = useSession();
+  const username = session?.user?.email || "default";
+  const gradioAppUrl = new URL(process.env.NEXT_PUBLIC_GRADIO_APP_URL || "https://openbio-calculator.hf.space");
+  gradioAppUrl.searchParams.set("username", username);
+
+  return (
+    <div className="container mx-auto p-10 h-screen">
+      <iframe src={gradioAppUrl.toString()} width="100%" height="100%"></iframe>
+    </div>
+  );
 }
