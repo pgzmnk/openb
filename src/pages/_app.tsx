03893726d8265ab1c276131971ca456e1ab139cb
pgzmnk <pgzmnk@gmail.com>
2023-07-19 09:18:59 -0700

    Successful build.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import useSWR from 'swr'
+
+
+const fetcher = (url: RequestInfo | URL) => fetch(url).then(r => r.json())
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const { data, error } = useSWR('/api/hello', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
   return (
     <Layout>
       <Component {...pageProps} />
---
+++ src/pages/api/project/[id].tsx
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    const { id } = req.query
+    res.end(`Post: ${id}`)
+}
