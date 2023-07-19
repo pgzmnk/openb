84e77ff33aaa98b781f082351b5d62644d082265
pgzmnk <pgzmnk@gmail.com>
2023-07-19 10:33:59 -0700

    Introduce base form.

---
+++ src/pages/project.tsx
@@ -0,0 +1,9 @@
+import CreateProjectForm from '@/components/CreateProjectForm';
+
+export default function Project() {
+    return (
+        <div>
+            <CreateProjectForm />
+        </div>
+    );
+}
